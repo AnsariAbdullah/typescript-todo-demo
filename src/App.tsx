11b12b0c86574d233a537b29278d0bd7cbfc7b98
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-02-10 16:40:53 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [listItems, setListItems] = useState<string[]>([]);
+	const [input, setInput] = useState<string>('car');
+
+	const handleChange = (e: any) => {
+		// console.log(e.target.value)
+		setInput(e.target.value)
+	}
+
+	const handleSubmit = () => {
+		setInput('');
+		setListItems([...listItems, input]);
+	}
+
+	const deleteItem = (index: number) => {
+		let newArr: string[];
+		newArr = listItems;
+		newArr.splice(index, 1);
+		console.log(newArr);
+		setListItems([...newArr])
+	}
+
+	return (
+		<div className="App">
+			<div className="controls">
+				<input
+					type="text"
+					className="input-box"
+					onChange={(e) => handleChange(e)}
+					value={input}
+					placeholder="add todo"
+				/>
+				<button
+					className="submit-button"
+					onClick={handleSubmit}
+				>
+					Add
+				</button>
+			</div>
+			<div className="todo">
+				<ul className="list-wrapper">
+					{listItems && listItems.map((item, index) =>
+						<li className="list-item" key={index}>
+							{item}
+							<span
+								onClick={() => deleteItem(index)}
+								className="close"
+							>
+								X
+							</span>
+						</li>
+					)}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
