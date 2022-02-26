49649057a7fe2507dba07a9cb1fa4b655c173d07
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-02-26 21:28:13 +0530

    add test cases

--- cypress/integration/todo_test.js
+++ cypress/integration/todo_test.js
@@ -1,15 +1,28 @@
 describe('Check todo', function (){
 	it('check blank input box', function(){
 		cy.visit('http://localhost:3000/')
+		cy.get('.input-box')
+			.type('bikes')
+			.should('have.value', 'bikes')
+		
 		cy.get('.submit-button').click()
 
 		// cy.pause()		this is going to pause the test like debugger
 
-		cy.get('.input-box')
-			.type('bikes')
-			.should('have.value', 'bikes')
+		
 
 		cy.get('.list-item')
-			.should('have.text', 'carX')
+			.should('have.text', 'bikesX')
+
+		cy.get('.input-box')
+			.type('cycle')
+			.should('have.value', 'cycle')
+		
+		cy.get('.submit-button').click()
+
+		cy.get('#close').click()
+
+		cy.get('.list-item')
+			.should('have.text', '')
 	})
 })
